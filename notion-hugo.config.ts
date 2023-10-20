a09676a1cbf51ab2323599610596e77143642829
sdockray <sean.patrick.dockray@gmail.com>
2023-10-20 16:21:09 +1100

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://sdockray.notion.site/Notion-DoIt-88da28e06f8447458eb20182c2cca97b?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
